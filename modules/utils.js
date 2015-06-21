9477d626c6abbbc231b3e3ef6ad2a8a5fd90d860
Alex Mejias <alexdmejias@gmail.com>
2015-06-20 20:18:36 -0400

    added number mapping function to utils

--- modules/utils.js
+++ modules/utils.js
@@ -5,5 +5,10 @@ module.exports = {
       min = 0;
     }
     return Math.floor(Math.random() * (max - min) + min);
+  },
+  // taken from the p5.js project
+  // https://github.com/processing/p5.js/blob/5c81d655f683f90452b80ab225a67e449463fff9/src/math/calculation.js#L394
+  map: function(n, start1, stop1, start2, stop2) {
+    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
   }
 };
