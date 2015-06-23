a0bf42a749ef91c9d1d5b5c4bb3b3b291dfd9393
Alex Mejias <alexdmejias@gmail.com>
2015-06-22 21:47:30 -0400

    updated modules

--- modules/utils.js
+++ modules/utils.js
@@ -1,22 +1,30 @@
-module.exports = {
-  range: function (min, max) {
-    if (!max) {
-      max = min;
-      min = 0;
-    }
-    return Math.floor(Math.random() * (max - min) + min);
-  },
-  // taken from the p5.js project
-  // https://github.com/processing/p5.js/blob/5c81d655f683f90452b80ab225a67e449463fff9/src/math/calculation.js#L394
-  map: function(n, start1, stop1, start2, stop2) {
-    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
-  },
+module.exports = function(cx, canvas) {
+  return {
+    cx : cx,
+    canvas: canvas,
+    range: function (min, max) {
+      if (!max) {
+        max = min;
+        min = 0;
+      }
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    // taken from the p5.js project
+    // https://github.com/processing/p5.js/blob/5c81d655f683f90452b80ab225a67e449463fff9/src/math/calculation.js#L394
+    map: function(n, start1, stop1, start2, stop2) {
+      return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+    },
 
-  getMousePos: function(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top
+    getMousePos: function(event) {
+      return {
+        x: event.clientX,
+        y: event.clientY
+      }
+    },
+
+    clear: function() {
+      this.cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+
   }
 };
